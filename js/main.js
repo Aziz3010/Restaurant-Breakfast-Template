8efec9836d539f70ee3bb604746c503ca3d016a3
unknown <a.abdelazizg@gmail.com>
2021-07-06 00:57:55 +0200

    uploud jquery for  scrolling

--- js/main.js
+++ js/main.js
@@ -9,13 +9,24 @@ $(".menuberger").click(function(){
     }
 })
 
+// Change navbar BG color
+$(window).scroll(function(){
+    let navbarTop = $("#Navbar").offset().top;
+    
+    if(navbarTop > 30){
+        $("#Navbar").css("background-color","rgba(160, 160, 160, 0.2)")        
+    }else{
+        $("#Navbar").css("background-color","transparent")
+    }
+})
 
+// Smoothly Scroll
+$(".leftnav ul li a , .rightnav ul li a , #Contact .footerlinks ul li a").click(function(e){
+    let currentLink = $(e.target).attr("href");
+    let sectionOffset = $(`${currentLink}`).offset().top;
+    
+    $("HTML , body").animate({scrollTop:sectionOffset},1500)
 
-
-
-
-
-
-
+})
 
 
